464e7bd08b6c03b450f493b41340bcab8d5d720e
Maged Elias <magedelias53@gmail.com>
2025-08-06 14:45:07 +0300

    V 1.1.3

--- dist/create_shortcut.py
+++ dist/create_shortcut.py
@@ -4,7 +4,7 @@ import os
 from win32com.client import Dispatch
 
 def create_shortcut(target_path, shortcut_name="QR Generator", icon_path=None):
-    desktop = os.path.join(os.environ["USERPROFILE"], "OneDrive", "Desktop")  # Or use expanduser("~/Desktop")
+    desktop = os.path.join(os.environ["USERPROFILE"], "OneDrive", "Desktop")  
     shortcut_path = os.path.join(desktop, f"{shortcut_name}.lnk")
     print(f"Creating shortcut at: {shortcut_path}")
 
@@ -18,10 +18,9 @@ def create_shortcut(target_path, shortcut_name="QR Generator", icon_path=None):
     print("Shortcut created successfully.")
 
 if __name__ == "__main__":
-    exe_path = os.path.abspath("dist/QR_full_app.exe")  # replace with your actual path
+    exe_path = os.path.abspath("dist/QR_full_app.exe")  
     print(f"EXE path: {exe_path}")
-    icon_path = r"C:/Users/COMPUMARTS/OneDrive/Desktop/Python Projects/QR Generator App/app_icon.ico"  # 👈 Use your icon path
+    icon_path = r"C:/Users/COMPUMARTS/OneDrive/Desktop/Python Projects/QR Generator App/QrCode.ico"  
     create_shortcut(exe_path, "QR Generator", icon_path)
 
-#  For desktop shortcut
-# #  here Ai also helped me for making a desktop shortcut.
+
